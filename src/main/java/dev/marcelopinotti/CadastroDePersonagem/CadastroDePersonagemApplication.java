2212aa096e35f1905f12d6f587c7956fb85bd4b3
marcelopinotti <marcelopinotti@outlook.com.br>
2025-09-22 14:14:37 -0300

    first commit

---
+++ src/main/java/dev/marcelopinotti/CadastroDePersonagem/CadastroDePersonagemApplication.java
@@ -0,0 +1,13 @@
+package dev.marcelopinotti.CadastroDePersonagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDePersonagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDePersonagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/marcelopinotti/CadastroDePersonagem/CadastroDePersonagemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcelopinotti.CadastroDePersonagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDePersonagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
