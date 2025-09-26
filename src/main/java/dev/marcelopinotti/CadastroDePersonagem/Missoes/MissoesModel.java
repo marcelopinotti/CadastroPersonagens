394d17a05c2d86694e2c57753711c9e2f63dac3a
marcelopinotti <marcelopinotti@outlook.com.br>
2025-09-25 22:01:46 -0300

    feat: put lombok in the class

--- src/main/java/dev/marcelopinotti/CadastroDePersonagem/Missoes/MissoesModel.java
+++ src/main/java/dev/marcelopinotti/CadastroDePersonagem/Missoes/MissoesModel.java
@@ -5,15 +5,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class MissoesModel {
 
     @Id
--- src/main/java/dev/marcelopinotti/CadastroDePersonagem/Personagens/PersonagemModel.java
+++ src/main/java/dev/marcelopinotti/CadastroDePersonagem/Personagens/PersonagemModel.java
@@ -8,13 +8,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 
 @Entity //transforma a classe em uma entidade
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data  // Todos os getters e setters
+@ToString
 public class PersonagemModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //gera o id automaticamente
@@ -28,53 +34,5 @@ public class PersonagemModel {
     @JoinColumn(name = "missoes_id") // nome da chave estrangeira
     private MissoesModel missoes;
 
-    public PersonagemModel() {
-    }
-    public PersonagemModel(Long id, String nome, String genero, int idade, String desenho) {
-        this.id = id;
-        this.nome = nome;
-        this.genero = genero;
-        this.idade = idade;
-        this.desenho = desenho;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getDesenho() {
-        return desenho;
-    }
-
-    public void setDesenho(String desenho) {
-        this.desenho = desenho;
-    }
 }
